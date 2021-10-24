5cd3400bd88524032407956872a34ee9ce20c64f
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-10-24 13:22:12 -0600

    Add home button to saved cover view

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ var coverImage = document.querySelector('.cover-image');
 var coverTitle = document.querySelector('.cover-title');
 var taglineOne = document.querySelector('.tagline-1');
 var taglineTwo = document.querySelector('.tagline-2');
-var randomButton = document.querySelector('.random-cover-button')
-
+var randomButton = document.querySelector('.random-cover-button');
+var formView = document.querySelector('.form-view');
+var makeNewButton = document.querySelector('.make-new-button');
+var homeView = document.querySelector('.home-view');
+var saveCoverButton = document.querySelector('.save-cover-button');
+var homeButton = document.querySelector('.home-button');
 
 // We've provided a few variables below
 var savedCovers = [
@@ -14,8 +18,18 @@ var currentCover;
 
 // Add your event listeners here 👇
 window.addEventListener('load', randomCover)
-
 randomButton.addEventListener('click', randomCover)
+makeNewButton.addEventListener('click', displayFormView)
+
+
+function displayFormView() {
+formView.classList.remove('hidden')
+homeView.classList.add('hidden')
+randomButton.classList.add('hidden')
+saveCoverButton.classList.add('hidden')
+homeButton.classList.remove('hidden')
+}
+
 
 // Create your event handlers and other functions here 👇
 function randomCover() {
