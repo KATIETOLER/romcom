288f4bc0b4c57d41f822082b62cb0fb51efd40b1
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-10-24 13:38:57 -0600

    Add hidden button functionality

--- src/main.js
+++ src/main.js
@@ -9,7 +9,8 @@ var makeNewButton = document.querySelector('.make-new-button');
 var homeView = document.querySelector('.home-view');
 var saveCoverButton = document.querySelector('.save-cover-button');
 var homeButton = document.querySelector('.home-button');
-
+var savedView = document.querySelector('.saved-view')
+var savedViewButton = document.querySelector('.view-saved-button')
 // We've provided a few variables below
 var savedCovers = [
   new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
@@ -20,6 +21,29 @@ var currentCover;
 window.addEventListener('load', randomCover)
 randomButton.addEventListener('click', randomCover)
 makeNewButton.addEventListener('click', displayFormView)
+savedViewButton.addEventListener('click', displayPageCovers)
+homeButton.addEventListener('click', displayHome)
+
+
+
+function displayHome() {
+  homeView.classList.remove('hidden')
+  formView.classList.add('hidden')
+  savedView.classList.add('hidden')
+  randomButton.classList.remove('hidden')
+  saveCoverButton.classList.remove('hidden')
+  homeButton.classList.add('hidden')
+}
+
+
+function displayPageCovers() {
+homeView.classList.add('hidden')
+formView.classList.add('hidden')
+savedView.classList.remove('hidden')
+randomButton.classList.add('hidden')
+saveCoverButton.classList.add('hidden')
+homeButton.classList.remove('hidden')
+}
 
 
 function displayFormView() {
